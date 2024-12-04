74a1aaefa89853d5fac75cae5dd66c7e8cf01474
Hussain Ahmad <hussainahmadd34@gmail.com>
2024-12-04 16:53:46 +0500

    Add new script file

---
+++ script.js
@@ -0,0 +1,7 @@
+const sum = () =>{
+    return(
+        <>
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui asperiores at voluptatum eos nisi libero beatae dolore doloremque quia aut illo, vel dolores accusamus eum neque aliquid tempora esse sed.</h1>
+        </>
+    )
+}
