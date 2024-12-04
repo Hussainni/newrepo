2a975133a21c2839c1c1134041bfd94657ecb06c
Hussain Ahmad <hussainahmadd34@gmail.com>
2024-12-04 18:11:49 +0500

    add new file here

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sum = () =>{
     return(
         <>
             <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui asperiores at voluptatum eos nisi libero beatae dolore doloremque quia aut illo, vel dolores accusamus eum neque aliquid tempora esse sed.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vitae, repudiandae ipsum ipsa tempora possimus sequi dolorum sit labore ullam! Illo voluptatem esse sequi repudiandae, nam numquam aliquid sint? Odit.</p>
         </>
     )
 }
